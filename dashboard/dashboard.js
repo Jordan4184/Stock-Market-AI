27499075eec3f9da4bdfe6e12337804a7371eab5
Jordan4184 <jordandonaldson14@gmail.com>
2024-07-03 08:07:48 -0700

    Initialized Dashboard.js APIs

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,9 +1,22 @@
-//Import chart.js, newsAPI,WebSockets, Alpha Vantage API, and the dotenv package
+//Import newsAPI.io or newsAPI.org for news data
+
+
+//Chart.js for Data Visualization
+const chart = require('chart.js');
+
+//dotenv for API keys: sets dotenv up in root directory
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+
+const AV_apiKey = process.env.Alpha_Vantage_API_Key; // console.log(!!AV_apiKey);
+const baseUrl = 'https://www.alphavantage.co/query'; // console.log(baseUrl); Use this to fetch data from the Alpha Vantage API.
+
+
 
 
 //Organization
 
 // Configuration and Initialization: Define API keys, base URLs, and any other global constants.
+
 // Utility Functions: Write reusable utility functions for fetching data and handling errors.
 // API Integration: Implement functions to interact with stock and news APIs.
 // Data Processing and Display: Write functions to update the DOM with the fetched data.
