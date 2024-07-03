93b449faad46fc7024de3c82b24a6e0077e043e0
Jordan4184 <jordandonaldson14@gmail.com>
2024-07-03 05:58:00 -0700

    Established JS

---
+++ dashboard/dashboard.js
@@ -0,0 +1,11 @@
+//Import chart.js, newsAPI,WebSockets, Alpha Vantage API, and the dotenv package
+
+
+//Organization
+
+// Configuration and Initialization: Define API keys, base URLs, and any other global constants.
+// Utility Functions: Write reusable utility functions for fetching data and handling errors.
+// API Integration: Implement functions to interact with stock and news APIs.
+// Data Processing and Display: Write functions to update the DOM with the fetched data.
+// Real-time Updates: Implement periodic updates for stock data.
+// Event Handlers and Initialization: Set up event handlers and initialize the application when the document is ready
