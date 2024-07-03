4f81a7772335c837fb872c7b23a79db98fb8d338
Jordan4184 <jordandonaldson14@gmail.com>
2024-07-03 16:59:40 -0700

    Time Functions Added

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,14 +1,12 @@
 //Import newsAPI.io or newsAPI.org for news data
 
-// Configuration and Initialization: Define API keys, base URLs, and any other global constants.
-
 //Chart.js for Data Visualization
 //const chart = require('chart.js');
 
-//dotenv for API keys: sets dotenv up in root directory
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+// config.js used for API key storage
+//require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
-const AV_apiKey = process.env.Alpha_Vantage_API_Key; // console.log(!!AV_apiKey);
+const AV_apiKey = Alpha_Vantage_API_Key; // console.log(!!AV_apiKey);
 const baseUrl = 'https://www.alphavantage.co/query'; // console.log(baseUrl); Use this to fetch data from the Alpha Vantage API.
 
 // Utility Functions: Write reusable utility functions for fetching data and handling errors.
@@ -33,16 +31,40 @@ async function fetchStockData (symbol) { //async used so that page does not have
 
 const stockSymbol = 'TSLA'; //Test call of symbol for fetchStockData to use
 
+//Custom Function to get previous business day
+function getPreviousBusinessDay(date) {
+    const day = new Date(date);
+    let offset;
+    if(date.getDay() === 0) { //Sunday
+        offset = 2;
+    } else if (day.getDay() === 1) { //Monday
+        offset = 3;
+    } else {
+        offset = 1;
+    }
+    day.setDate(day.getDate() - offset);
+    return date.toISOString().split('T')[0]; //Returns dat in YYYY-MM-DD format
+}
+
 function displayStockData(data) { //Defines stock data function and extracts data
     if(!data) {
         console.log('No data availible'); //Error handling
         return;
     }
-    //Extract relevant data
+
+    //Extract relevant data from data object
     const timeSeries = data['Time Series (Daily)']; //Accesses the Time SEries (daily) object from fetched data
     const latestDate = Object.keys(timeSeries)[0]; //Extract the latest data possible, returns array of dates(keys) in the time series object. [0] gets first date
     const latestData = timeSeries[latestDate]; //Use the latest date key to get stock data from time series object
-    const closePrice = latestData['4, close']; //Extracts the closing price at the latest date. Data contains (open, high, low, close, prices) fields, using close
+    const closePrice = latestData['4. close']; //Extracts the closing price at the latest date. Data contains (open, high, low, close, volume) fields, using close index 4
+
+    //Fetch date and yesterday's date
+    const today = new Date().toISOString().split('T')[0]; //Need to split at T and index at 0 because ISO String outputs date and time denoted T seperaing the values
+    const yesterday = getPreviousBusinessDay(today); //Uses custom function above to find yesterdays date
+
+    //Check if todays data is availible, if not use yesterday's data
+
+
 
     //Fetches specified data 
     document.getElementById('stock-symbol').textContent = stockSymbol; //Finds ID and replaces txt. Updates HTML elements with ID's 'stock-symbol' etc...
@@ -50,7 +72,7 @@ function displayStockData(data) { //Defines stock data function and extracts dat
     document.getElementById('stock-date').textContent = `Date: ${latestDate}`;
 }
 
-fetchStockData(stockSymbol)
+fetchStockData(stockSymbol) //calls the ticker symbol and has additional error handling if incorrectly run
     .then(data => {
         if (data) {
             displayStockData(data);
@@ -66,7 +88,7 @@ fetchStockData(stockSymbol)
 
 
 
-//Organization
+//Next Steps:
 
 // API Integration: Implement functions to interact with stock and news APIs.
 // Data Processing and Display: Write functions to update the DOM with the fetched data.
